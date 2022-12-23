5792983ae1c56185267788ffb6cb2a2dfa9c9cc1
Huandong Chang <huandongchang@hotmail.com>
2022-12-23 00:20:39 -0500

    First commit

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Popcorn Dataset
+#'
+#' A dataset containing the pop rate experiments under different conditions
+#'
+#' @format
+#' A data frame with 32 rows and 5 columns:
+#' \describe{
+#'   \item{Brand}{Brand name}
+#'   \item{Microwave}{Microwave location}
+#'   \item{Time}{Microwave time}
+#'   \item{Unpopped..percent.}{Unpopped percent}
+#'   \item{Poprate}{Popped percent}
+#' }
+"Popcorn"
